4a7fc30d1a35bbef518f3c4c63b1dba62d1d693c
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-04 17:58:05 -0300

    Create maven project

---
+++ Lucas Nascimento/api/src/main/java/io/github/lucasnr/turingtodo/TuringTodoApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasnr.turingtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringTodoApplication.class, args);
+	}
+
+}
