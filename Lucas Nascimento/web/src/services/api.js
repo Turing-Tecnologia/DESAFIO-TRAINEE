d2f53ce563ea4e7af4012984d3f618e5e3fab84a
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-05 16:00:11 -0300

    Create create task feature

--- Lucas Nascimento/web/src/services/api.js
+++ Lucas Nascimento/web/src/services/api.js
@@ -19,3 +19,6 @@ export const getUserByToken = (token) =>
   });
 
 export const updateUser = (user) => api.patch(`/users/${user.id}`, { ...user });
+
+export const createTask = (task, userId) =>
+  api.post(`/users/${userId}/tasks`, { ...task });
